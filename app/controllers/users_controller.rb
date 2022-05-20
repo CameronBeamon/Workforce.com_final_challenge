b59000997b14854790c4c5e9d1ab3395424e7a0a
Cameron Beamon <beamoncameron@gmail.com>
2022-05-20 17:22:52 -0500

    fixed delete method in organisation controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
     def update
         @user = User.find_by(id: session[:user_id])
-        # @user.organisation_id = params[:id]
-        pp @user
         if @user.update_attribute(:organisation_id, params[:id])
           redirect_to root_path
         else 
@@ -11,8 +9,7 @@ class UsersController < ApplicationController
     end
     def delete
         user = User.find_by(id: session[:user_id])
-        user.organisation_id = nil
-        if user.save
+        if  user.update_attribute(:organisation_id, nil)
           redirect_to root_path
         else
             render json: {message: "user did not save"}
