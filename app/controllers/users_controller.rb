3ea480a9a37955e3c30d9f6c0cbde6f39be94efd
Cameron Beamon <beamoncameron@gmail.com>
2022-05-20 18:46:41 -0500

    added feature where all shifts are deleted upon leaving an organisation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,10 @@ class UsersController < ApplicationController
     end
     def delete
         user = User.find_by(id: session[:user_id])
-        if  user.update_attribute(:organisation_id, nil)
+        if  user.update_attribute(:organisation_id, nil) && user.shifts.delete_all
           redirect_to root_path
         else
-            render json: {message: "user did not save"}
+            render json: {message: "user did not save or shifts did not delete"}
         end
     end
 end
