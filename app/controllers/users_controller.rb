4d7d76debe3037ff0d7e5f8e94927f77db490565
Cameron Beamon <beamoncameron@gmail.com>
2022-05-20 17:19:26 -0500

    fixed organisation id update problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
     def update
         @user = User.find_by(id: session[:user_id])
-        @user.organisation_id = params[:id]
-        if @user.save
+        # @user.organisation_id = params[:id]
+        pp @user
+        if @user.update_attribute(:organisation_id, params[:id])
           redirect_to root_path
         else 
             render json: {message: "user did not save"}
+        end
     end
     def delete
         user = User.find_by(id: session[:user_id])
